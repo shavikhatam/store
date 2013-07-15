3dd5180a7b1bef0881051de2e6688ac14b6e2c61
Shavkatjon Khatamov <Shavik@MacBook-Pro-Shavkatjon.local>
2013-07-15 19:04:52 +0400

    добавил удаление заказа

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -41,4 +41,10 @@ class OrdersController < ApplicationController
 
   end
 
+  def destroy
+    order = Order.find params[:id]
+    order.destroy
+    redirect_to orders_path, notice: 'order deleted'
+  end
+
 end
